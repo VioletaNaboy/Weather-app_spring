eead47e782a2908853d9fd2017ccd57f11d3bacd
VioletaNaboy <vnaboishchykova@gmail.com>
2023-08-19 16:56:34 +0200

    deleted_gitignore_v1.2

--- src/test/java/com/example/demo/WeatherApplicationTests.java
+++ src/test/java/com/example/demo/WeatherApplicationTests.java
@@ -13,7 +13,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 @SpringBootTest
 class WeatherApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
