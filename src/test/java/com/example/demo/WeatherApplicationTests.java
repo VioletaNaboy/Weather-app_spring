1e4ca0aeb23538068fa87f6ae3e09a07d25b9b2b
VioletaNaboy <vnaboishchykova@gmail.com>
2023-08-19 16:48:43 +0200

    added_test_v1.2

--- src/test/java/com/example/demo/WeatherApplicationTests.java
+++ src/test/java/com/example/demo/WeatherApplicationTests.java
@@ -1,8 +1,16 @@
 package com.example.demo;
-
+import com.example.demo.domain.WeatherData;
+import com.example.demo.service.WeatherService;
+import com.example.demo.repository.Impls.OpenWeatherRepositoryImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 @SpringBootTest
 class WeatherApplicationTests {
 
@@ -10,4 +18,29 @@ class WeatherApplicationTests {
 	void contextLoads() {
 	}
 
+}
+class WeatherServiceTest {
+
+	@InjectMocks
+	private WeatherService weatherService;
+
+	@Mock
+	private OpenWeatherRepositoryImpl openWeatherRepositoryImpl;
+
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	public void testGetWeatherByCity() {
+		String cityName = "New York";
+		WeatherData weatherData = new WeatherData(cityName, "Sunny", 25.0, 60.0);
+		when(openWeatherRepositoryImpl.getWeatherByCity(cityName)).thenReturn(weatherData);
+
+		WeatherData result = weatherService.getWeatherByCity(cityName);
+
+		assertEquals(weatherData, result);
+		verify(openWeatherRepositoryImpl, times(1)).getWeatherByCity(cityName);
+	}
 }
